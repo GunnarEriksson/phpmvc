dd0275eedc03bb381818e362bfdb774b54eed8f6
Gunnar Eriksson <gunnar.t.eriksson@gmail.com>
2016-07-16 19:37:37 +0200

    Updated syntax for forms

--- kmom02/app/view/comment/deleteForm.tpl.php
+++ kmom02/app/view/comment/deleteForm.tpl.php
@@ -1,16 +1,16 @@
 <h2>Ta bort kommentar</h2>
 <div class='comment-form'>
-    <form method=post>
-        <input type=hidden name="redirect" value="<?=$this->url->create("$pageKey")?>">
-        <input type=hidden name="pageKey" value='<?=$pageKey?>'>
-        <input type=hidden name="id" value='<?=$id?>'>
+    <form method="post">
+        <input type="hidden" name="redirect" value="<?=$this->url->create("$pageKey")?>">
+        <input type="hidden" name="pageKey" value='<?=$pageKey?>'>
+        <input type="hidden" name="id" value='<?=$id?>'>
         <fieldset>
         <legend>Ta bort kommentaren</legend>
         <p><label>Kommentar:<br/><textarea name='content' readonly><?=$content?></textarea></label></p>
         <p><label>Namn:<br/><input type='text' name='name' value='<?=$name?>' readonly/></label></p>
         <p><label>Hemsida:<br/><input type='text' name='web' value='<?=$web?>' readonly/></label></p>
         <p><label>E-post:<br/><input type='text' name='mail' value='<?=$mail?>' readonly/></label></p>
-        <p class=buttons>
+        <p class='buttons'>
             <input type='submit' name='doDelete' value='Radera' onClick="this.form.action = '<?=$this->url->create('comment/delete')?>'"/>
             <input type='submit' name='doRemoveAll' value='Radera allt' onClick="this.form.action = '<?=$this->url->create('comment/remove-all')?>'"/>
         </p>
--- kmom02/app/view/comment/editForm.tpl.php
+++ kmom02/app/view/comment/editForm.tpl.php
@@ -1,16 +1,16 @@
 <h2>Ändra kommentar</h2>
 <div class='comment-form'>
-    <form method=post>
-        <input type=hidden name="redirect" value="<?=$this->url->create("$pageKey")?>">
-        <input type=hidden name="pageKey" value='<?=$pageKey?>'>
-        <input type=hidden name="id" value='<?=$id?>'>
+    <form method="post">
+        <input type="hidden" name="redirect" value="<?=$this->url->create("$pageKey")?>">
+        <input type="hidden" name="pageKey" value='<?=$pageKey?>'>
+        <input type="hidden" name="id" value='<?=$id?>'>
         <fieldset>
         <legend>Ändra en kommentar</legend>
         <p><label>Kommentar:<br/><textarea name='content'><?=$content?></textarea></label></p>
         <p><label>Namn:<br/><input type='text' name='name' value='<?=$name?>'/></label></p>
         <p><label>Hemsida:<br/><input type='text' name='web' value='<?=$web?>'/></label></p>
         <p><label>E-post:<br/><input type='text' name='mail' value='<?=$mail?>'/></label></p>
-        <p class=buttons>
+        <p class='buttons'>
             <input type='submit' name='doEdit' value='Spara' onClick="this.form.action = '<?=$this->url->create('comment/edit')?>'"/>
             <input type='reset' value='Rensa'/>
         </p>
