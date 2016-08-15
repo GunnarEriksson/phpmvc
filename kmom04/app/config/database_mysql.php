1a0be6cee6f5912b18abb41c8f8b21bf8afcc63c
Gunnar Eriksson <gunnar.t.eriksson@gmail.com>
2016-08-15 14:30:24 +0200

    Handles both local and extern server

--- kmom04/app/config/database_mysql.php
+++ kmom04/app/config/database_mysql.php
@@ -1,16 +1,27 @@
 <?php
+if (isset($_SERVER['REMOTE_ADDR'])) {
+    if ($_SERVER['REMOTE_ADDR'] == '::1') {
+        return [
+            'dsn'             => "mysql:host=localhost;dbname=phpmvc;",
+            'username'        => "root",
+            'password'        => "",
+            'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
+            'table_prefix'    => "guer_",
 
-return [
-    // Set up details on how to connect to the database
-    'dsn'             => "mysql:host=localhost;dbname=phpmvc;",
-    'username'        => "root",
-    'password'        => "",
-    'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
-    'table_prefix'    => "guer_",
+            // Display details on what happens
+            //'verbose' => true,
 
-    // Display details on what happens
-    //'verbose' => true,
-
-    // Throw a more verbose exception when failing to connect
-    //'debug_connect' => 'true',
-];
+            // Throw a more verbose exception when failing to connect
+            //'debug_connect' => 'true',
+        ];
+    } else {
+        define('DB_PASSWORD', 'uiJ7A6:g');
+        return [
+            'dsn'             => "mysql:host=blu-ray.student.bth.se;dbname=guer16;",
+            'username'        => "guer16",
+            'password'        => DB_PASSWORD,
+            'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
+            'table_prefix'    => "guer_",
+        ];
+    }
+}
